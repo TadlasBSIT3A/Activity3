71c4aadc1b63608862d011e29fc4c5eae14a15dc
TadlasBSIT3A <143198653+TadlasBSIT3A@users.noreply.github.com>
2023-10-12 16:50:39 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,144 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+
+const Main = () => {
+  const [main, setmain] = useState(false);
+  const [flight, setflight] = useState(false);
+  const [counter, setcounter] = useState(false);
+  const [back, setBack] = useState(false);
+
+  const tomain = () => {
+    main ? setmain(false) : setmain(true);
+    flight ? setflight(false) : setflight(false);
+    counter ? setcounter(false) : setcounter(false);
+    back ? setBack(false) : setBack(false);
+  };
+  const toflight = () => {
+    flight ? setflight(false) : setflight(true);
+    counter ? setcounter(false) : setcounter(false);
+    main ? setmain(false) : setmain(true);
+    back ? setBack(false) : setBack(true);
+  };
+  const tocounter = () => {
+    flight ? setflight(false) : setflight(false);
+    counter ? setcounter(false) : setcounter(true);
+    main ? setmain(false) : setmain(true);
+    back ? setBack(false) : setBack(true);
+  };
+
+  return (
+    <View style={styles.heads}>
+      <View style={styles.head}>
+        <Button title="FLASHLIGHT" color="#2c2c54" disabled={main} onPress={toflight} />
+        <Button title="COUNTER" color="#2c2c54" disabled={main} onPress={tocounter} />
+      </View>
+      <View style={styles.body}>
+        {flight && <Flight />}
+
+        {counter && <Counter />}
+      </View>
+      <View style={styles.btn}>
+        {back && <Button title="BACK" onPress={tomain} color="#2c2c54" />}
+      </View>
+    </View>
+  );
+};
+
+const Flight = () => {
+  const [image, setimage] = useState(false);
+  const [text, setText] = useState("ON");
+
+  const toImage = () => {
+    image ? setimage(false) : setimage(true);
+    if (text === 'ON') {
+      setText('OFF');
+    } else {
+      setText('ON');
+    }
+  };
+
+  return (
+    <View>
+      {image &&  <Image
+            source={{uri : 'https://img.icons8.com/?size=80&id=EomHyNB0IOtX&format=png'}}
+            style={{ width: 150, height: 150, resizeMode: "stretch" , marginVertical: 20}}
+          />
+          }
+        {!image &&  <Image
+            source={{uri : 'https://img.icons8.com/?size=80&id=CNNNkWTst3cf&format=png'}}
+            style={{ width: 150, height: 150, resizeMode: "stretch" ,  marginVertical: 20}}
+          />
+          }
+     <Button title= {text} onPress={toImage} color="#2c2c54" />
+    </View>
+  );
+};
+const Counter = () => {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <View style={styles.main}>
+      <Text style={styles.number}>{number}</Text>
+      <View style={styles.btnmain}>
+        <Button
+          title="-1" color="#2c2c54"
+          onPress={() => {
+            setNumber(number - 1);
+          }}
+        />
+        <Button
+          title="+1" color="#2c2c54"
+          onPress={() => {
+            setNumber(number + 1);
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Main />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  head: {
+    flexDirection: "row",
+    gap: 20,
+    justifyContent: "space-between",
+    paddingVertical: 20,
+  },
+  heads: {
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  number: {
+    fontSize: 100,
+    fontWeight: 'bold'
+  },
+  main: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 400,
+  },
+  btnmain: {
+    flexDirection: "row",
+    gap: 20,
+  },
+  btn: {
+    width: 100,
+    paddingVertical: 20
+  },
+});
